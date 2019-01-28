02c5542834c27b7beba52308cd95a9f294e5ec74
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-01-28 16:45:33 +0100

    created StudentSwingViewTest

---
+++ com.examples.school/src/test/java/com/examples/school/view/swing/StudentSwingViewTest.java
@@ -0,0 +1,15 @@
+package com.examples.school.view.swing;
+
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.runner.RunWith;
+
+@RunWith(GUITestRunner.class)
+public class StudentSwingViewTest extends AssertJSwingJUnitTestCase {
+
+	@Override
+	protected void onSetUp() {
+		
+	}
+
+}
