3cbd604ba2d6279df92b5f2a0ca515187c7406ce
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-01-28 16:59:04 +0100

    initial AssertJ Swing test setup

--- com.examples.school/src/test/java/com/examples/school/view/swing/StudentSwingViewTest.java
+++ com.examples.school/src/test/java/com/examples/school/view/swing/StudentSwingViewTest.java
@@ -1,15 +1,31 @@
 package com.examples.school.view.swing;
 
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(GUITestRunner.class)
 public class StudentSwingViewTest extends AssertJSwingJUnitTestCase {
 
+	private FrameFixture window;
+
+	private StudentSwingView studentSwingView;
+
 	@Override
 	protected void onSetUp() {
-		
+		GuiActionRunner.execute(() -> {
+			studentSwingView = new StudentSwingView();
+			return studentSwingView;
+		});
+		window = new FrameFixture(robot(), studentSwingView);
+		window.show(); // shows the frame to test
 	}
 
+	@Test
+	public void test() {
+		// just to check the setup works
+	}
 }
