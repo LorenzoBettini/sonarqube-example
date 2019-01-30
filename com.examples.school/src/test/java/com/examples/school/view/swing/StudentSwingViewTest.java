c3e39b3467271dae88168db6815ea7b24af8db8f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-01-30 13:55:04 +0100

    failing test for the whole ui

--- com.examples.school/src/test/java/com/examples/school/view/swing/StudentSwingViewTest.java
+++ com.examples.school/src/test/java/com/examples/school/view/swing/StudentSwingViewTest.java
@@ -1,6 +1,7 @@
 package com.examples.school.view.swing;
 
 import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.core.matcher.JLabelMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
@@ -30,5 +31,11 @@ public class StudentSwingViewTest extends AssertJSwingJUnitTestCase {
 	public void testControlsInitialStates() {
 		window.label(JLabelMatcher.withText("id"));
 		window.textBox("idTextBox").requireEnabled();
+		window.label(JLabelMatcher.withText("name"));
+		window.textBox("nameTextBox").requireEnabled();
+		window.button(JButtonMatcher.withText("Add")).requireDisabled();
+		window.list("studentList");
+		window.button(JButtonMatcher.withText("Delete Selected")).requireDisabled();
+		window.label("errorMessageLabel").requireText(" ");
 	}
 }
