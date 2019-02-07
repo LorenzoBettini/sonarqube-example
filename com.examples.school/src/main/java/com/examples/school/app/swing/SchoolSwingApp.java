1e9abe54e50fc78a305e04f4ab6a2daaed629631
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-07 09:31:08 +0100

    main application with picocli command line parser

--- com.examples.school/src/main/java/com/examples/school/app/swing/SchoolSwingApp.java
+++ com.examples.school/src/main/java/com/examples/school/app/swing/SchoolSwingApp.java
@@ -1,6 +1,7 @@
 package com.examples.school.app.swing;
 
 import java.awt.EventQueue;
+import java.util.concurrent.Callable;
 
 import com.examples.school.controller.SchoolController;
 import com.examples.school.repository.mongo.StudentMongoRepository;
@@ -8,34 +9,45 @@ import com.examples.school.view.swing.StudentSwingView;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
-public class SchoolSwingApp {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-	/**
-	 * Launch the application.
-	 */
+@Command(mixinStandardHelpOptions = true)
+public class SchoolSwingApp implements Callable<Void> {
+
+	@Option(names = { "--mongo-host" }, description = "MongoDB host address")
+	private String mongoHost = "localhost";
+
+	@Option(names = { "--mongo-port" }, description = "MongoDB host port")
+	private int mongoPort = 27017;
+
+	@Option(names = { "--db-name" }, description = "Database name")
+	private String databaseName = "school";
+
+	@Option(names = { "--db-collection" }, description = "Collection name")
+	private String collectionName = "student";
+
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					String mongoHost = "localhost";
-					int mongoPort = 27017;
-					if (args.length > 0)
-						mongoHost = args[0];
-					if (args.length > 1)
-						mongoPort = Integer.parseInt(args[1]);
-					MongoClient mongoClient = new MongoClient(new ServerAddress(mongoHost, mongoPort));
-					StudentMongoRepository studentRepository = new StudentMongoRepository(mongoClient);
-					StudentSwingView studentView = new StudentSwingView();
-					SchoolController schoolController = new SchoolController(studentView, studentRepository);
-					studentView.setSchoolController(schoolController);
-					studentView.setVisible(true);
-					schoolController.allStudents();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+		CommandLine.call(new SchoolSwingApp(), args);
+	}
+
+	@Override
+	public Void call() throws Exception {
+		EventQueue.invokeLater(() -> {
+			try {
+				StudentMongoRepository studentRepository = new StudentMongoRepository(
+						new MongoClient(new ServerAddress(mongoHost, mongoPort)), databaseName, collectionName);
+				StudentSwingView studentView = new StudentSwingView();
+				SchoolController schoolController = new SchoolController(studentView, studentRepository);
+				studentView.setSchoolController(schoolController);
+				studentView.setVisible(true);
+				schoolController.allStudents();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		});
+		return null;
 	}
 
 }
