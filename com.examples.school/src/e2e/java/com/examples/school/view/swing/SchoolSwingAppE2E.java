b4a0febc1fcf28641e6e2d0d64edd5b91cec4375
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-07 21:56:04 +0100

    created E2E test

---
+++ com.examples.school/src/e2e/java/com/examples/school/view/swing/SchoolSwingAppE2E.java
@@ -0,0 +1,77 @@
+package com.examples.school.view.swing;
+
+import static org.assertj.swing.launcher.ApplicationLauncher.*;
+
+import javax.swing.JFrame;
+
+import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.finder.WindowFinder;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.bson.Document;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.testcontainers.containers.GenericContainer;
+
+import com.mongodb.MongoClient;
+
+@RunWith(GUITestRunner.class)
+public class SchoolSwingAppE2E extends AssertJSwingJUnitTestCase {
+
+	@SuppressWarnings("rawtypes")
+	@ClassRule
+	public static final GenericContainer mongo =
+		new GenericContainer("mongo:4.0.5") 
+			.withExposedPorts(27017);
+
+	private static final String DB_NAME = "test-db";
+	private static final String COLLECTION_NAME = "test-collection";
+
+	private MongoClient mongoClient;
+
+	private FrameFixture window;
+
+	@Override
+	protected void onSetUp() {
+		String containerIpAddress = mongo.getContainerIpAddress();
+		Integer mappedPort = mongo.getMappedPort(27017);
+		mongoClient = new MongoClient(containerIpAddress, mappedPort);
+		// always start with an empty database
+		mongoClient.getDatabase(DB_NAME).drop();
+		// add some students to the database
+		addTestStudentToDatabase("1", "first student");
+		addTestStudentToDatabase("2", "second student");
+		// start the Swing application
+		application("com.examples.school.app.swing.SchoolSwingApp")
+			.withArgs(
+				"--mongo-host=" + containerIpAddress,
+				"--mongo-port=" + mappedPort.toString(),
+				"--db-name=" + DB_NAME,
+				"--db-collection=" + COLLECTION_NAME
+			)
+			.start();
+		// get a reference of its JFrame
+		window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
+			@Override
+			protected boolean isMatching(JFrame frame) {
+				return "Student View".equals(frame.getTitle()) && frame.isShowing();
+			}
+		}).using(robot());
+	}
+
+	@Override
+	protected void onTearDown() {
+		mongoClient.close();
+	}
+
+	private void addTestStudentToDatabase(String id, String name) {
+		mongoClient
+			.getDatabase(DB_NAME)
+			.getCollection(COLLECTION_NAME)
+			.insertOne(
+				new Document()
+					.append("id", id)
+					.append("name", name));
+	}
+}
