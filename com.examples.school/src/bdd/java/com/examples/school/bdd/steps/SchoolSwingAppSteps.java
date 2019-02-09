066775468424117af7067a66d751d3c5a2927a7e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-09 15:36:21 +0100

    created steps file with copied methods

---
+++ com.examples.school/src/bdd/java/com/examples/school/bdd/steps/SchoolSwingAppSteps.java
@@ -0,0 +1,27 @@
+package com.examples.school.bdd.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SchoolSwingAppSteps {
+
+	@Given("The database contains a student with id {string} and name {string}")
+	public void the_database_contains_a_student_with_id_and_name(String string, String string2) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("The Student View is shown")
+	public void the_Student_View_is_shown() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@Then("The list contains an element with id {string} and name {string}")
+	public void the_list_contains_an_element_with_id_and_name(String string, String string2) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+}
